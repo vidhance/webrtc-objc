ddf8f8f0539fd1c21f3a1d3e2d8488e8f2ec1939
Thomas Fanell <thomas.fanell@vidhance.com>
2025-04-04 11:11:22 +0200

    Add initial `Package.swift` file.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [
+    	.iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/vidhance/webrtc-objc/releases/download/M114/WebRTC.framework.zip",
+            checksum: "245a5663d11d21464d6202f0fc389b35020c20080be2bad462f85a556218546f"
+        ),
+    ]
+)
